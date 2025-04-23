923d40257044d32735ddc18a5fbb278ee500e5b4
maraduranclark <mara_duran-clark@brown.edu>
2025-04-23 15:09:28 -0400

    Update Final_Project.py
    Imported pygames, trying to fix bugs and sending pygame install code to Paris

--- Final_Project.py
+++ Final_Project.py
@@ -1,2 +1,52 @@
-# This is a simple test script
-print("hello, world")
+# Import Modules
+import os
+import pygame as pg
+
+if not pg.font:
+    print("Warning, fonts disabled")
+if not pg.mixer:
+    print("Warning, sound disabled")
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, "data")
+
+# pygame setup
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+clock = pg.time.Clock()
+running = True
+dt = 0
+
+player_pos = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    pg.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pg.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pg.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pg.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pg.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pg.quit()
