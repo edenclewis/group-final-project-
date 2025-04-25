25292089eba79c65bc3e5b89df1ae4f8aa41376b
maraduranclark <mara_duran-clark@brown.edu>
2025-04-25 13:56:49 -0400

    Merge branch 'main' of https://github.com/edenclewis/group-final-project-

--- Final_Project.py
+++ Final_Project.py
@@ -12,16 +12,24 @@ GREY = (200,200,200)
 #set up the screen 
 SCREEN_WIDTH = 560
 SCREEN_HEIGHT = 560
-GRID_COLS = 7  # 
-GRID_ROWS = 7  # 7 rows of tiles
+GRID_COLS = 12  # 
+GRID_ROWS = 8  # 7 rows of tiles
 BLANK_BOX_WIDTH = 500
 tile_size = 80
 SCREEN_WIDTH = (GRID_COLS * tile_size) + BLANK_BOX_WIDTH
 SCREEN_HEIGHT = GRID_ROWS * tile_size
 # create the screen
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-## initailizing grid with zeros 
-matrix_grid = [[0 for _ in range(GRID_COLS)] for _ in range(GRID_ROWS)]
+
+#creating the matrix with white (0) and black (1) squares
+matrix_grid_des = [[1,1,1,1,1,0,1,1,1,1,1,1], 
+                   [0,0,0,0,0,0,0,1,1,1,1,1], 
+                   [1,0,1,0,1,0,0,0,0,0,1,1],
+                   [0,0,0,0,0,0,0,1,1,1,1,1], 
+                   [1,1,1,0,0,0,0,0,0,1,1,1], 
+                   [1,1,1,0,1,0,0,1,1,1,1,1], 
+                   [1,0,0,0,0,1,0,0,0,0,0,0],
+                   [1,1,1,1,1,1,0,1,1,1,1,1]]
 
 def draw_grid(tile_size):
     ## fill screen 
@@ -29,7 +37,7 @@ def draw_grid(tile_size):
     for row in range(GRID_ROWS):
         for col in range(GRID_COLS):
             rect = pygame.Rect(col * tile_size, row * tile_size, tile_size, tile_size)
-            if matrix_grid[row][col] == 1:
+            if matrix_grid_des[row][col] == 1:
                 pygame.draw.rect(SCREEN, BLACK, rect)
             else:
                 pygame.draw.rect(SCREEN, WHITE, rect)
@@ -39,19 +47,7 @@ def draw_grid(tile_size):
 blank_box = pygame.Rect(GRID_COLS * tile_size,0, BLANK_BOX_WIDTH, SCREEN_HEIGHT)
 pygame.draw.rect(SCREEN, GREY, blank_box)
 ## drawing parts of the crossword --> try to make this part of the code more efficent 
-matrix_grid[0][0] = 1
-matrix_grid[0][1] = 1
-matrix_grid[0][5] = 1
-matrix_grid[0][6] = 1
-matrix_grid[1][0] = 1
-matrix_grid[5][0] = 1
-matrix_grid[6][0] = 1
-matrix_grid[6][1] = 1
-matrix_grid[6][5] = 1
-matrix_grid[6][6] = 1
-matrix_grid[5][6] = 1
-matrix_grid[1][6] = 1
-matrix_grid[3][3] = 1
+
 
 # Add event handling to the main loop
 run = True
